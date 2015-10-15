98a17e38c8e7f42e7a6c059847a75f2a2a49daec
Kevin Alberts <kevin@kevinalberts.nl>
2015-10-15 20:01:48 +0200

    Version bump & Author change

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='IrkerNotifcationPlugin',
-    version='0.1',
+    version='0.2',
     description='Plugin to announce Trac changes via Irker',
-    author='Southen',
+    author='Kurocon',
     url='https://github.com/Southen/trac-irker-plugin',
     license='BSD',
     packages=['irker_notification'],
